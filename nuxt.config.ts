b24c8e5d9ca24840e51e494a5668b00aec8d07ca
ぷらてぃ君 <nagonago561@yahoo.co.jp>
2019-05-01 22:30:31 +0900

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -84,12 +84,12 @@ const config: NuxtConfiguration = {
     }
   },
   env: {
-    F_APIKEY: process.env.F_AUTHDOMAIN || 'AIzaSyAkzTehaWaWTS0TcdQw26ijaz7ufQuQ1pM',
+    F_APIKEY: process.env.F_AUTHDOMAIN || '',
     F_AUTHDOMAIN: process.env.F_AUTHDOMAIN || 'boardgame-management.firebaseapp.com',
     F_DATABASEURL: process.env.F_DATABASEURL || 'https://boardgame-management.firebaseio.com',
     F_PROJECTID: process.env.F_PROJECTID || 'boardgame-management',
     F_STORAGEBUCKET: process.env.F_STORAGEBUCKET || 'boardgame-management.appspot.com',
-    F_MESSAGINGSENDERID: process.env.F_MESSAGINGSENDERID || '249026475741'
+    F_MESSAGINGSENDERID: process.env.F_MESSAGINGSENDERID || ''
   },
 
 };
